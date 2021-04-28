fbdd825c7f30cd0e63b1b0fab12113e7a9f0b4de
allisondurham <allison.durham@gmail.com>
2021-04-28 12:44:32 -0400

    thing works

--- game.py
+++ game.py
@@ -3,7 +3,7 @@
 
 import pygame
 import random
-# from blob import Blob
+from blob import Blob
 
 STARTING_BLUE_BLOBS = 10
 STARTING_RED_BLOBS = 3
@@ -18,28 +18,6 @@ game_display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Blob World")
 clock = pygame.time.Clock()
 
-
-class Blob:
-
-    def __init__(self, color):
-        self.x = random.randrange(0, WIDTH)
-        self.y = random.randrange(0, HEIGHT)
-        self.size = random.randrange(4,8)
-        self.color = color
-
-    def move(self):
-        self.move_x = random.randrange(-1,2)
-        self.move_y = random.randrange(-1,2)
-        self.x += self.move_x
-        self.y += self.move_y
-
-        if self.x < 0: self.x = 0
-        elif self.x > WIDTH: self.x = WIDTH
-        
-        if self.y < 0: self.y = 0
-        elif self.y > HEIGHT: self.y = HEIGHT
-
-
 def draw_environment(blob_list):
     game_display.fill(WHITE)
 
@@ -53,8 +31,8 @@ def draw_environment(blob_list):
     
 
 def main():
-    blue_blobs = dict(enumerate([Blob(BLUE) for i in range(STARTING_BLUE_BLOBS)]))
-    red_blobs = dict(enumerate([Blob(RED) for i in range(STARTING_RED_BLOBS)]))
+    blue_blobs = dict(enumerate([Blob(BLUE, WIDTH, HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
+    red_blobs = dict(enumerate([Blob(RED, WIDTH, HEIGHT) for i in range(STARTING_RED_BLOBS)]))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -64,4 +42,8 @@ def main():
         clock.tick(60)
 
 if __name__ == '__main__':
-    main()
+    blue_blobs = dict(enumerate([Blob(BLUE,WIDTH,HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
+    red_blobs = dict(enumerate([Blob(RED,WIDTH,HEIGHT) for i in range(STARTING_RED_BLOBS)]))
+    while True:    
+
+        main()
