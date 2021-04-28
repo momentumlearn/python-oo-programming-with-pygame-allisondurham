7ad7b511bd83793bdfc043d94181482dd41c52cd
allisondurham <allison.durham@gmail.com>
2021-04-28 12:01:35 -0400

    blobs working

--- game.py
+++ game.py
@@ -3,21 +3,24 @@
 
 import pygame
 import random
+# from blob import Blob
+
+STARTING_BLUE_BLOBS = 10
+STARTING_RED_BLOBS = 3
 
 WIDTH = 800
 HEIGHT = 600
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
-RED = (225, 0, 0)
+RED = (255, 0, 0)
 
 game_display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Blob World")
 clock = pygame.time.Clock()
 
-# while True:
-#     pass
 
 class Blob:
+
     def __init__(self, color):
         self.x = random.randrange(0, WIDTH)
         self.y = random.randrange(0, HEIGHT)
@@ -25,37 +28,40 @@ class Blob:
         self.color = color
 
     def move(self):
-        self.move_x = random.randrange(-1, 2)
-        self.move_y = random.randrange(-1, 2)
+        self.move_x = random.randrange(-1,2)
+        self.move_y = random.randrange(-1,2)
         self.x += self.move_x
         self.y += self.move_y
 
         if self.x < 0: self.x = 0
         elif self.x > WIDTH: self.x = WIDTH
-
+        
         if self.y < 0: self.y = 0
         elif self.y > HEIGHT: self.y = HEIGHT
 
 
-def draw_environment(blob):
+def draw_environment(blob_list):
     game_display.fill(WHITE)
-    pygame.draw.circle(game_display, blob.color, [blob.x, blob.y], blob.size)
+
+    for blob_dict in blob_list:
+        for blob_id in blob_dict:
+            blob = blob_dict[blob_id]
+            pygame.draw.circle(game_display, blob.color, [blob.x, blob.y], blob.size)
+            blob.move()
+
     pygame.display.update()
-    blob.move()
+    
 
 def main():
-    red_blob = Blob(RED)
+    blue_blobs = dict(enumerate([Blob(BLUE) for i in range(STARTING_BLUE_BLOBS)]))
+    red_blobs = dict(enumerate([Blob(RED) for i in range(STARTING_RED_BLOBS)]))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
-        draw_environment(red_blob)
+        draw_environment([blue_blobs,red_blobs])
         clock.tick(60)
 
-if __name__== "__main__" :
+if __name__ == '__main__':
     main()
-
-# while True:
-#     pass
